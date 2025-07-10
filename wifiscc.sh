c592006c56e7f1d441c3aa4a825cd2828e05339b
nniell90 <nniell@sentineledge.io>
2025-07-10 14:25:23 -0500

    Update wifiscc.sh

--- wifiscc.sh
+++ wifiscc.sh
@@ -4,7 +4,9 @@ nmcli dev wifi connect "sccguest"
 
 #sleep 300
 
-read -t 300 -p "Waiting 5 minutes... (press enter to skip) " || true
+#read -t 300 -p "Waiting 5 minutes... (press enter to skip) " || true
+
+ping -c 300 127.0.0.1 > /dev/null
 
 echo "sleeping for 5 minutes"
 
